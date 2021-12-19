2e68731c43879de2cc28721474a5cf5507f34906
alexandernair <alexnair@me.com>
2021-12-19 15:03:05 -0600

    got backend working with mongoose

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,17 @@ import express from "express";
 
 import connectDB from "../config/database";
 import user from "./routes/api/user";
-
+import cors from "cors";
 
 const app = express();
+// app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-// Connect to MongoDB
+// Connect to MongoDB yay :) changing
 connectDB();
 
 // Express configuration
@@ -22,7 +28,7 @@ app.get("/", (_req, res) => {
   res.send("API Running");
 });
 
-app.use("/api/user", user);
+app.use("/api/park", user);
 
 const port = app.get("port");
 const server = app.listen(port, () =>
